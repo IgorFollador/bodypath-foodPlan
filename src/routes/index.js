e0553abeb88245c07f56045aead160225a42ea3a
Igor Follador <igorledf@hotmail.com>
2022-06-22 22:04:27 -0300

    Initial Structure

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser');
+
+module.exports = app => {
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+        next();
+    });
+
+    app.use(bodyParser.json());
+    
+    app.get('/', (req, res) => res.send('Physical Evaluation Microservice'));
+}
